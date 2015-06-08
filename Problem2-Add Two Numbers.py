ee849cce55ebbd00e55db903522d23bd76d2e850
liuqianchao <liuqianchao@163.com>
2015-06-09 00:23:33 +0800

    add problem2

---
+++ Problem2-Add Two Numbers.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} l1
+    # @param {ListNode} l2
+    # @return {ListNode}
+    def addTwoNumbers(self, l1, l2):
+        def toint(node):
+            return node.val + 10 * toint(node.next) if node else 0
+        def tolist(n):
+            node = ListNode(n % 10)
+            if n > 9:
+                node.next = tolist(n / 10)
+            return node
+        return tolist(toint(l1) + toint(l2))
