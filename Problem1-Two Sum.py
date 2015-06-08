df5f38ba58caadec36d963ab9160c7ad0165ce8e
QianchaoLiu <liuqianchao@163.com>
2015-06-08 23:05:04 +0800

    Update Problem1-Two Sum.py

--- Problem1-Two Sum.py
+++ Problem1-Two Sum.py
@@ -14,3 +14,21 @@ class Solution:
                     list.append(index+1)
                     return list
                     break
+
+#another answer with a lower efficiency
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[]}
+    def twoSum(self, nums, target):
+            newlist=[]
+            answers = []
+            for flag, num in enumerate(nums):
+                newlist.append(num)
+                if newlist.__contains__(target - num):
+                    i = newlist.index(target - num)
+                    if i<flag:
+                        answers.append(i+1)
+                        answers.append(flag+1)
+                        return answers
+                        break
